31aba14348e852315a3d040d1e48a50de9fe3c04
Techgamr <60068447+Techgamr@users.noreply.github.com>
2026-03-28 11:58:01 +0000

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  description = "frontend for mcapi";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    inputs@{ flake-parts, ... }:
+    # https://flake.parts/module-arguments.html
+    flake-parts.lib.mkFlake { inherit inputs; } (
+      top@{
+        config,
+        withSystem,
+        moduleWithSystem,
+        ...
+      }:
+      {
+        imports = [
+          # Optional: use external flake logic, e.g.
+          # inputs.foo.flakeModules.default
+        ];
+        flake = {
+          # Put your original flake attributes here.
+        };
+        systems = [
+          # systems for which you want to build the `perSystem` attributes
+          "x86_64-linux"
+          "aarch64-linux"
+        ];
+        perSystem =
+          { config, pkgs, ... }:
+          {
+            devShells.default = pkgs.mkShell {
+              buildInputs = with pkgs; [
+                packwiz
+              ];
+            };
+          };
+      }
+    );
+}
